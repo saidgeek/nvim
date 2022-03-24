ad616510bbbd30a7d3477e316980c87e5e0fd5e0
saidgeek <me@saidgeek.land>
2022-03-24 03:16:15 -0300

    feat: add shortcuts

--- lua/basics.lua
+++ lua/basics.lua
@@ -23,3 +23,26 @@ vim.o.showmode = false
 vim.o.showtabline = 2
 vim.o.signcolumn = 'yes'
 vim.o.mouse = 'a'
+
+vim.api.nvim_set_keymap("n", "<C-W>", ":w<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-Q>", ":q<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<C-WQ>", ":wq<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-N>', ":Lexplore<CR> :vertical resize 30<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>t", ":sp<CR> :term<CR> :resize 20N<CR> i", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<C-Q>", "<C-\\><C-n> :q<CR>", {noremap = true, silent = true})
+
+vim.g["netrw_banner"] = 0
+vim.g["netrw_liststyle"] = 3
+vim.g["netrw_winsize"] = 25
